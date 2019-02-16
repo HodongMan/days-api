675b8791062a3dc3ee21c059e0dfa617c5b6a6cd
hodongman <jhd9206@gmail.com>
2019-02-16 10:55:23 +0900

    server api application init

---
+++ src/main/java/com/hodongman/days/DaysApplication.java
@@ -0,0 +1,14 @@
+package com.hodongman.days;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaysApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/hodongman/days/DaysApplicationTests.java
@@ -0,0 +1,17 @@
+package com.hodongman.days;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DaysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
